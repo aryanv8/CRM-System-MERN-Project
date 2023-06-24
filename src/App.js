3d9707edf258f0dc8543d541e908679b814a4154
Aryan Vishwakarma <aryan.vishwakarma0902@gmail.com>
2023-06-24 23:42:08 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import './App.css';
+import "./App.css";
 
 // import of libraries
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import {HashRouter as Router, Routes, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// import of react libraries
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // import of components
-import { RedButton, BlueButton, TempButton, RedOutlineButton, WhiteOutlineButton } from './components/TempButton';
-import Navbar from './components/Navbar';
-
+import {
+  RedButton,
+  BlueButton,
+  TempButton,
+  RedOutlineButton,
+  WhiteOutlineButton,
+} from "./components/TempButton";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-
-<Router>
-        
+    <div className="App" style={{height: '200vh'}}>
+      <Router>
         <Navbar />
-        
-        <Routes>
-          <Route path='/'  />
-          <Route path='/projects'  />
-          <Route path='/skills'  />
-          <Route path='/contact' />
-        </Routes>
 
+        <Routes>
+          <Route path="/" />
+          <Route path="/projects" />
+          <Route path="/skills" />
+          <Route path="/contact" />
+        </Routes>
       </Router>
 
-      <RedButton >Ok</RedButton>
-      <BlueButton >Stop</BlueButton>
+      <RedButton>Ok</RedButton>
+      <BlueButton>Stop</BlueButton>
       <TempButton colorClass="btn-info">Temp Info</TempButton>
       <TempButton color="rgb(255, 255, 255)">Temp</TempButton>
       {/* Tempbutton with color yellow in rgb */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
-
-  const [expandNavbar,setExpandNavbar] = useState(false);
+  const [expandNavbar, setExpandNavbar] = useState(false);
   const location = useLocation();
 
-  useEffect(()=>{
+  useEffect(() => {
     setExpandNavbar(false);
-  },[location])
-    
+  }, [location]);
+
   return (
-    <div class="navbar navbar-dark bg-dark navbar-expand-md py-3 px-3" id={expandNavbar ? "open" : "close"}>
-        <Link to='/' className='navbar-brand h1'> <i class="fa-solid fa-users-gear fa-xl" style={{color: "#f0f2f5;"}}></i> CRM-System</Link>
-        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar" onClick={()=>{
-                setExpandNavbar((prev) => !prev);
-            }}>
+    <div
+      class="navbar navbar-dark navbar-expand-md py-3 px-3 sticky-top"
+      id={expandNavbar ? "open" : "close"}
+      style={{backgroundColor: "rgba(0, 0, 0, 0.5)"}}
+    >
+      <Link to="/" className="navbar-brand h1">
+        {" "}
+        <i
+          class="fa-solid fa-users-gear fa-xl"
+          style={{ color: "#f0f2f5;" }}
+        ></i>{" "}
+        CRM-System
+      </Link>
 
-            <span class="navbar-toggler-icon"></span>
-        </button>
+      <button
+        class="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbar"
+        onClick={() => {
+          setExpandNavbar((prev) => !prev);
+        }}
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="navbar-collapse collapse mr-0 " id="navbar" style={{justifyContent: 'flex-end'}}>
-            <div className='navbar-nav'>
-              <Link to='/' className='nav-link mx-3'>Home</Link>
-              <Link to='/Projects' className='nav-link mx-3'>Projects</Link>
-              <Link to='/login' className=' nav-link mx-3'>Login</Link> 
-              <Link to='/signup' className='nav-link mx-3'>Sign-Up</Link> 
-            </div>
+      <div
+        class="navbar-collapse collapse mr-0 "
+        id="navbar"
+        style={{ justifyContent: "flex-end" }}
+      >
+        <div className="navbar-nav">
+          <Link to="/" className="nav-link mx-3">
+            Home
+          </Link>
+          <Link to="/Projects" className="nav-link mx-3">
+            Projects
+          </Link>
+          <Link to="/login" className=" nav-link mx-3">
+            Login
+          </Link>
+          <Link to="/signup" className="nav-link mx-3">
+            Sign-Up
+          </Link>
         </div>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
