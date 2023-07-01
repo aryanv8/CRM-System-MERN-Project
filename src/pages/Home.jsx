8822779e127b2dc8222269141a47dac0c3dab75c
GOKULNATH V <kvggokulnathv@gmail.com>
2023-07-01 14:18:19 +0530

    forgotpwd added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../styles/Home.css';
 function Home(){
+    var [slo,seTslo]=useState('');
+    var tem="Good customer relations equals success";
 return(<div><br></br><div className="home">
-    <h1>home</h1></div>
+    <h1 className="text-uppercase font-italic">{tem}</h1>
+   
+
+    </div>
 </div>);
 }
 export default Home;
