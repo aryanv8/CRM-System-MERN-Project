61e1e495eaa62c69e9d9470edcd2601736aa89b9
GOKULNATH V <kvggokulnathv@gmail.com>
2023-07-02 22:03:29 +0530

    home added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,8 +23,8 @@ function Home(){
  <img class="img3 border-radius-rounded" src="https://img.freepik.com/free-vector/stock-market-analysis-with-chart_23-2148584739.jpg?size=626&ext=jpg&ga=GA1.2.1950086803.1687017148&semt=ais" alt="visualize"/>
  <h2 class="text-uppercase">Visualisation of Customers Information</h2>
     </div>
-    <br/>
-    <h3><button class="btn btn-outline-light"> <Link to="/contact" class="nav-link">Contact</Link></button> us for any clarification</h3></div>);},1900);
+    
+    <h3><br/><button class="btn btn-outline-light"> <Link to="/contact" class="nav-link">Contact</Link></button> us for any clarification</h3></div>);},1900);
 
 return(<div className="home container-fluid">
     <br/>
