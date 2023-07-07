d4ce0ea71e8a1c35dc14b7f106d11da2dd0392c1
Aryan Vishwakarma <aryan.vishwakarma0902@gmail.com>
2023-07-08 00:03:42 +0530

    Updated

--- src/admin/FeedBackShow.jsx
+++ src/admin/FeedBackShow.jsx
@@ -9,7 +9,6 @@ function FeedBackShow() {
   const [resData, setResData] = useState([]);
   const [rec, setRec] = useState([]);
 
-
   useEffect(() => {
     //backend Axios using
     setRec(location.state.name);
@@ -28,48 +27,77 @@ function FeedBackShow() {
   const [search, setSearch] = useState("");
 
   return (
-    <div className='container-fluid table-responsive-sm customers'>
-      <h2 style={{textAlign:"center"}} className='mt-3 mb-4 text-center text-white'>Feedback Page</h2>
+    <div className="container-fluid table-responsive-sm customers">
+      <h2
+        style={{ textAlign: "center" }}
+        className="mt-3 mb-4 text-center text-white"
+      >
+        Feedback Page
+      </h2>
       <span className="d-flex float-end">
         <label for="search">Search here:</label>
-        <input className="me-2 form-control" type="search" defaultValue={rec} placeholder="Search..." aria-label="Search" id='search' onChange={(e) => setSearch(e.target.value)}/>
+        <input
+          className="me-2 form-control"
+          type="search"
+          defaultValue={rec}
+          placeholder="Search..."
+          aria-label="Search"
+          id="search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </span>
-      <div className="col-sm-5 col-md-6 col-12 pb-4 mx-auto" >
-      {resData.filter((item)=>{
-        
-            if(search == ""){
-              return item
+      <div className="col-sm-5 col-md-6 col-12 pb-4 mx-auto">
+        {resData
+          .filter((item) => {
+            if (search == "") {
+              return item;
+            } else if (item.productid.includes(search)) {
+              return item;
+            } else if (
+              item.productname.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return item;
+            } else if (
+              item.username.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return item;
             }
-            else if(item.productid.includes(search)){
-              return item
-            }
-            else if(item.productname.toLowerCase().includes(search.toLowerCase())){
-              return item
-            }
-            else if(item.username.toLowerCase().includes(search.toLowerCase())){
-              return item
-            }
-          }).map((data)=>(
-                <div>
-            <div style={{borderRadius:"30px"}} className="text-justify darkerfeed mt-4 nth">
-                    <h4 id="pn" style={{textAlign:"left"}}>{data.username}</h4>
-                    <div class="row">
-                    <p class="col-md-6 col-sm-12">Product id:{data.productid}</p>
-                    <p class="col-md-6 col-sm-12">Product Name:{data.productname}</p>
-                    </div>
-                    <div class="row">
-                    <p class="col-md-3 col-sm-12">value of money:{data.value_for_money}</p>
-                    <p class="col-md-3 col-sm-12">easy of use:{data.ease_of_use}</p>
-                    <p class="col-md-3 col-sm-12">experience:{data.experience}</p>
-                    <p class="col-md-3 col-sm-12">durability:{data.durability}</p>
-                    </div>
-                    <p>Over all Rating: {data.overall_rating}</p>
-                    <p>{data.suggestions_or_compaints}</p>
-                    <p style={{textAlign:"right"}}>{data.timestamp}</p>
-                </div><br /></div>
-
-            ))}</div>
-            <br/><br/>
+          })
+          .map((data) => (
+            <div>
+              <div
+                style={{ borderRadius: "30px" }}
+                className="text-justify darkerfeed mt-4 nth"
+              >
+                <h4 id="pn" style={{ textAlign: "left" }}>
+                  {data.username}
+                </h4>
+                <div class="row">
+                  <p class="col-md-6 col-sm-12">Product id:{data.productid}</p>
+                  <p class="col-md-6 col-sm-12">
+                    Product Name:{data.productname}
+                  </p>
+                </div>
+                <div class="row">
+                  <p class="col-md-3 col-sm-12">
+                    value of money:{data.value_for_money}
+                  </p>
+                  <p class="col-md-3 col-sm-12">
+                    easy of use:{data.ease_of_use}
+                  </p>
+                  <p class="col-md-3 col-sm-12">experience:{data.experience}</p>
+                  <p class="col-md-3 col-sm-12">durability:{data.durability}</p>
+                </div>
+                <p>Over all Rating: {data.overall_rating}</p>
+                <p>{data.suggestions_or_compaints}</p>
+                <p style={{ textAlign: "right" }}>{data.timestamp}</p>
+              </div>
+              <br />
+            </div>
+          ))}
+      </div>
+      <br />
+      <br />
     </div>
   );
 }
