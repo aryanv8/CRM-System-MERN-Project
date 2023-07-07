b32e3d6ab8da2650de7b07f5a117b33e2090dfce
Aryan Vishwakarma <aryan.vishwakarma0902@gmail.com>
2023-07-07 10:02:54 +0530

    Added descriptions

--- constants.js
+++ constants.js
@@ -1,11 +1,20 @@
-
-export const SERVER = 'http://localhost:4000'
+/**
+ * @description Path to the server
+ * @type {string}
+ * @default "http://localhost:4000"
+ */
+export const SERVER = "http://localhost:4000";
 
 /**
  * @description Path to the user server
  * @type {string}
  * @default "http://localhost:4000/users"
  */
-export const USER_SERVER = `${SERVER}/users`
+export const USER_SERVER = `${SERVER}/users`;
 
-export const ADMIN_SERVER = `${SERVER}/admin`
+/**
+ * @description Path to the admin server
+ * @type {string}
+ * @default "http://localhost:4000/admin"
+ */
+export const ADMIN_SERVER = `${SERVER}/admin`;
